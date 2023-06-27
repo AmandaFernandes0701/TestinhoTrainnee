bc4bd2312d88ed2c291b850f53745042dc7dc897
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-06-27 12:08:33 -0300

    feat: first commit test

--- testetinho/src/App.jsx
+++ testetinho/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Amandinha Fofa</h1>
+      <h1>Amandinha Fofinha</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
